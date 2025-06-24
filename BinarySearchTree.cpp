845dda7f812c54bf12c35a9f46bff42dfac54229
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-06-24 10:15:04 +0700

    membuat left dan right new node point ke NULL

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -38,6 +38,10 @@ public:
 
         // Step 2: Assign value to the data field of new node
         newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point ot NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
     }
 };
 
