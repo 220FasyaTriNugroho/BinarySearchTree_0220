11e9a71a7dcfa79a47928a4b338952e4df94faf7
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-06-24 10:41:51 +0700

    untuk cek apakah pohon kosong

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -141,6 +141,12 @@ public:
         postorder(ptr->rightchild);
         cout << ptr->info << " ";
     }    
+
+    bool isEmpty()
+    {
+        // Checks if the tree is empty
+        return ROOT == nullptr;
+    }
 };
 
 
